4d913b3fbeb8a6eeeaad6428b6f945c5ab200b8e
elladelverne <44614381+elladelverne@users.noreply.github.com>
2020-11-30 14:12:17 -0500

    Started Fake Data it Works

--- client.js
+++ client.js
@@ -1,1 +1,70 @@
-document.querySelector("p").style.color = "red"
+console.log(loadData())
+
+
+function loadData() {
+    //JSON - JavaScript Object Notation
+    const movies = [
+    
+        
+    {
+        //key : value
+        "name" : "Braveheart",
+        "genre" : "Action",
+        "reviews" : [
+            {
+                "rating" : 5,
+                "like" : true,
+                "user" : {
+                    "name" : "Ella DelVerne",
+                    "gender" : "female",
+                    "region" : "USA"
+                }
+            },
+            {
+                "rating" : 2,
+                "like" : false,
+                "user" : {
+                    "name" : "Lily DelVerne",
+                    "gender" : "female",
+                    "region" : "USA"
+                } 
+            }
+        ]
+    },    
+        
+        
+    {
+        //key : value
+        "name" : "Gladiator",
+        "genre" : "Action",
+        "reviews" : [
+            {
+                "rating" : 5,
+                "like" : true,
+                "user" : {
+                    "name" : "Ella DelVerne",
+                    "gender" : "female",
+                    "region" : "USA"
+                }
+            },
+            {
+                "rating" : 1,
+                "like" : false,
+                "user" : {
+                    "name" : "Lily DelVerne",
+                    "gender" : "female",
+                    "region" : "USA"
+                }
+            }
+        ]
+    }
+     
+        
+    ]
+    
+    
+    return{
+        "movies" : movies
+    } 
+    
+}
