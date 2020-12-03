5713582b1a9da3ba9a969477f0dffa6a7e8dce40
elladelverne <44614381+elladelverne@users.noreply.github.com>
2020-12-03 13:21:01 -0500

    Add files via upload

--- client.js
+++ client.js
@@ -1,1 +1,87 @@
-document.querySelector("p").style.color = "red"
+const data = loadData()
+console.log(data)
+
+const $moviesList = document.getElementById("moviesList")
+
+renderMovies()
+
+function renderMovies(){
+    data.movies.forEach(movie => {
+         const $movie = document.createElement("div")
+         $movie.innerHTML = `<h1>${movie.name}</h1>`
+         movie.reviews.forEach(review => {
+            $movie.innerHTML += `<li>${review.rating}</li>`
+         })
+         $moviesList.append($movie)
+    })
+    
+}
+
+
+function loadData() {
+    //JSON - JavaScript Object Notation
+    const movies = [
+    
+        
+    {
+        //key : value
+        "name" : "Braveheart",
+        "genre" : "Action",
+        "reviews" : [
+            {
+                "rating" : 5,
+                "like" : true,
+                "user" : {
+                    "name" : "Ella DelVerne",
+                    "gender" : "female",
+                    "region" : "USA"
+                }
+            },
+            {
+                "rating" : 2,
+                "like" : false,
+                "user" : {
+                    "name" : "Lily DelVerne",
+                    "gender" : "female",
+                    "region" : "USA"
+                } 
+            }
+        ]
+    },    
+        
+        
+    {
+        //key : value
+        "name" : "Gladiator",
+        "genre" : "Action",
+        "reviews" : [
+            {
+                "rating" : 5,
+                "like" : true,
+                "user" : {
+                    "name" : "Ella DelVerne",
+                    "gender" : "female",
+                    "region" : "USA"
+                }
+            },
+            {
+                "rating" : 1,
+                "like" : false,
+                "user" : {
+                    "name" : "Lily DelVerne",
+                    "gender" : "female",
+                    "region" : "USA"
+                }
+            }
+        ]
+    }
+     
+        
+    ]
+    
+    
+    return{
+        "movies" : movies
+    } 
+    
+}
